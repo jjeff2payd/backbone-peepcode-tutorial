3e873ede5b7bdd5018514d065d616cc94952f96b
Joao S. López <jslopez@csrg.inf.utfsm.cl>
2012-05-20 20:09:49 -0400

    Understanding routers
    Commands:
        App.navigate('blank', true)
        App.navigate('', true)

--- backbone-tunes/public/js/Tunes.js
+++ backbone-tunes/public/js/Tunes.js
@@ -76,4 +76,34 @@
 
     });
 
+    window.BackboneTunes = Backbone.Router.extend({
+        routes: {
+            '': 'home',
+            'blank': 'blank'
+        },
+
+        initialize: function() {
+            this.libraryView = new LibraryView({
+                collection: window.library
+            });
+        },
+
+        home: function() {
+            var $container = $('#container');
+            $container.empty();
+            $container.append(this.libraryView.render().el);
+        },
+
+        blank: function() {
+            $('#container').empty();
+            $('#container').text('blank');
+        }
+    });
+
+    $(function() {
+        window.App = new BackboneTunes();
+        Backbone.history.start();
+        // { pushState:true } Use traditional URLs instead of the hashmark
+    });
+
 })(jQuery);
